e195c0cf7584b6ed6c682e1639a4bb18965cc300
AsaharaMasahiro <citron1605@gmail.com>
2025-02-04 17:34:26 +0900

    fix: rive

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import fs from "fs-extra";
+
+// ビルド後にRiveファイルをコピーする
+const copyRiveFiles = () => {
+  return {
+    name: "copy-rive-files",
+    closeBundle: async () => {
+      await fs.copy("src/assets/rive", "dist/assets/rive", { overwrite: true });
+    }
+  };
+};
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), copyRiveFiles()],
   publicDir: "public",
   assetsInclude: ["**/*.riv"],
   build: {
-    assetsInlineLimit: 0,
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo?.name?.endsWith(".riv")) {
-            return "assets/[name][extname]";
-          }
-          return "assets/[name]-[hash][extname]";
-        }
-      }
-    }
+    assetsInlineLimit: 0
   },
   server: {
     fs: {
